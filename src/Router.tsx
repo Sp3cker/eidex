bec363d863407f2fb855b4be200f2ace5e28e0ba
Sp3cker <speker97@protonmail.com>
2025-05-18 09:57:00 -0500

    fix : Router base path

--- src/Router.tsx
+++ src/Router.tsx
@@ -4,7 +4,7 @@ import { Route, Router, Switch } from "wouter";
 const AppRouter = () => {
   return (
     <div className="flex min-h-screen justify-center bg-zinc-800">
-      <Router base="/eidex">
+      <Router base="/">
         <Switch>
           <Route path="/" component={Map} />
         </Switch>
