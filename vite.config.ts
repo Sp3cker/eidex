259daaf256dfb5a6fb331189fcc2662135928f04
Sp3cker <speker97@protonmail.com>
2025-05-18 12:12:08 -0500

    feat: cloudflare worker

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { cloudflare } from "@cloudflare/vite-plugin";
+
 import tailwindcss from '@tailwindcss/postcss'
 import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), cloudflare()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
