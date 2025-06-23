de85d896c9b3b3a400f0f7465528025b400f5cf6
Sp3cker <speker97@protonmail.com>
2025-06-22 21:19:20 -0500

    Delete evo folder

---
+++ src/utils/determineEvoCap.ts
@@ -0,0 +1,32 @@
+import caps from "@/data/caps.json";
+import {pokemonData} from '@/data/pokemon'
+import { parseShortEvolutions } from "./parseEvo";
+/** Mons with these */
+const nonLevelupMethods =[0,
+5,
+6,
+7,
+21,
+22,
+23,
+26,
+27,
+29,
+31,
+32,
+35,
+36,
+37,
+38,
+39,
+40,
+41,
+43,
+44,
+47,
+48,
+49,
+50,
+51,
+52]
+
