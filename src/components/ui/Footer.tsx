f87336afe7ea943877d9219fb72fbeb5a3e3b11f
Sp3cker <94161757+Sp3cker@users.noreply.github.com>
2025-11-01 10:24:04 -0500

    Update Footer.tsx

--- src/components/ui/Footer.tsx
+++ src/components/ui/Footer.tsx
@@ -54,7 +54,7 @@ const Footer = memo(function Footer() {
           Data for EI 1.3.1
         </p>
         <p className="font-pkmnem leading-xs text-sm/3 text-white">
-          At least Emma's ass is nice
+          konnichiwagwan
         </p>
       </div>
       <div className="flex items-center justify-end gap-5">
