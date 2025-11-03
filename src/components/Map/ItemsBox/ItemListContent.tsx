2b70947c3457c85da53b4037fbef95bb211426c2
Sp3cker <speker97@protonmail.com>
2025-11-02 21:43:33 -0600

    Play withfonts

--- src/components/Map/ItemsBox/ItemListContent.tsx
+++ src/components/Map/ItemsBox/ItemListContent.tsx
@@ -4,15 +4,15 @@ import { Item, ItemWithAmount } from "@/data/map";
 const ItemListRender = (showPrice: boolean) => (item: ItemWithAmount) => (
   <hgroup className="min-w-0">
     <div className="flex min-w-0 flex-row items-start justify-between gap-2">
-      <h3 className="min-w-0 flex-1 break-words pl-1 text-xs/4 font-bold md:text-sm">
+      <h3 className="min-w-0 flex-1 break-words pl-1 text-sm/4 font-bold md:text-sm">
         {(item as { name?: string }).name || "Unnamed"}
       </h3>
-      <p className="font-pkmnem text-shadow-xs shrink-0 pr-4 text-lg leading-4">
+      <p className="font-pkmnem text-shadow-xs shrink-0 pr-4 leading-4">
         {showPrice ? "$" + item.price : "x" + item.amount}
       </p>
     </div>
     {(item as { description?: string }).description && (
-      <p className="font-pkmnem text-shadow-2xs break-words pl-1 leading-3 md:leading-4">
+      <p className="font-pkmnem text-shadow-2xs break-words pl-1 text-sm leading-3 md:leading-4">
         {(item as { description?: string }).description}
       </p>
     )}
